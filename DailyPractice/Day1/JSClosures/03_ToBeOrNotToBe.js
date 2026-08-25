fc0d88444a46782fcb1cded5d2bb45af29f0eddc
sripilla <120908523+sripilla@users.noreply.github.com>
2026-08-25 17:12:34 +0530

    Update 03_ToBeOrNotToBe.js

--- DailyPractice/Day1/JSClosures/03_ToBeOrNotToBe.js
+++ DailyPractice/Day1/JSClosures/03_ToBeOrNotToBe.js
@@ -21,17 +21,28 @@ Example 3:
 Input: func = () => expect(5).notToBe(null)
 Output: {"value": true}
 Explanation: 5 !== null so this expression returns true.
-*/
+var expect= function(val){
+    return {
+        toBe: function(otherval){
+        if (val===otherval){
+            return true;
+        }
+        else{
+            throw new Error("Not Equal");
+        }
+        },
 
-/**
- * @param {string} val
- * @return {Object}
- */
-var expect = function(val) {
+        notToBe: function(otherVal) {
+            if (val !== otherVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
     
 };
+   
+};
 
-/**
- * expect(5).toBe(5); // true
- * expect(5).notToBe(5); // throws "Equal"
- */
+console.log(expect(5).toBe(5));     
+console.log(expect(5).notToBe(null));  
