e50c688ec158bbda3d93a27facd01f8739dd3c40
sripilla <120908523+sripilla@users.noreply.github.com>
2026-08-25 16:30:18 +0530

    Update 01_HelloWorld.js

--- DailyPractice/Day1/JSClosures/01_HelloWorld.js
+++ DailyPractice/Day1/JSClosures/01_HelloWorld.js
@@ -28,20 +28,15 @@ Constraints:
 0 <= args.length <= 10
 
 
-*/
 
 
-/**
- * @return {Function}
- */
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        
-    }
+var createHelloWorld=function(){
+    return function(...args){
+        return "Hello World";
+    };
 };
+const f = createHelloWorld();
+console.log(f());
+        
 
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
+
