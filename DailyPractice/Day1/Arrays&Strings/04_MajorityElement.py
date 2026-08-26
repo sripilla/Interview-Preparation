38e83ee9924f0fbde6dc7d0fc1463ef60f594f3b
sripilla <120908523+sripilla@users.noreply.github.com>
2026-08-26 10:54:22 +0530

    Add majority element finder with user input
    Implemented a majority element finder that takes user input for the array size and elements.

--- DailyPractice/Day1/Arrays&Strings/04_MajorityElement.py
+++ DailyPractice/Day1/Arrays&Strings/04_MajorityElement.py
@@ -22,8 +22,20 @@ n == nums.length
 -109 <= nums[i] <= 109
 The input is generated such that a majority element will exist in the array.
  
-"""
+arr1=[]
 
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        
+m= int(input("Enter the size of the array:"))
+
+print(f"Enter {m} elements of the first array:")
+for i in range(m):
+    arr1.append(int(input()))
+
+for i in range(m):
+    count =0
+    for j in range(m):
+        if arr1[i]==arr1[j]:
+            count+=1
+
+    if count >= m//2:
+        print("Majority element:", arr1[i])
+        break
