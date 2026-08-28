d63b47a4d9f60873f460d0eb1dae7c263506b157
sripilla <120908523+sripilla@users.noreply.github.com>
2026-08-28 10:28:55 +0530

    Update 01_ArrayTransform.js

--- DailyPractice/Day2/JSArrays/01_ArrayTransform.js
+++ DailyPractice/Day2/JSArrays/01_ArrayTransform.js
@@ -40,6 +40,15 @@ fn returns an integer.
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-    
+var map= function(arr, fn){
+    let result=[]
+    for (let i=0; i<arr.length; i++){
+        result.push(fn(arr[i], i));
+    }
+    return result;
 };
+var plusone= function(n){
+return n+1;
+};
+
+console.log(map([1, 2, 3], plusone))
