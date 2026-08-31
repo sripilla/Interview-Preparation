58e31124265f303c2345717cc7cac4d02ca51f0e
sripilla <120908523+sripilla@users.noreply.github.com>
2026-08-31 20:18:33 +0530

    Add function to find second largest element
    Implemented function to find the second largest element in an array.

--- DailyPractice/Day3/Arrays/02_SecondLargestSmallest.py
+++ DailyPractice/Day3/Arrays/02_SecondLargestSmallest.py
@@ -17,12 +17,30 @@ Second Largest = 7
 Second Smallest = 2
 """
 
-class Solution:
-    def findSecond(self, nums: list[int]) -> tuple[int, int]:
-        # Track smallest, second smallest
-        
-        # Track largest, second largest
-        
-        # Traverse the array
-        
-        pass
+def secmaxele(arr):
+    lar = arr[0]
+    seclar = arr[1]
+
+    if seclar > lar:
+        lar, seclar = seclar, lar
+
+    for i in range(len(arr)):
+        if arr[i] > lar:
+            seclar = lar
+            lar = arr[i]
+
+        elif arr[i] > seclar and arr[i] != lar:
+            seclar = arr[i]
+
+    return seclar
+
+
+arr = []
+n = int(input("Enter the size of the array:"))
+
+print(f"Enter {n} elements:")
+
+for i in range(n):
+    arr.append(int(input()))
+
+print("Second largest element:", secmaxele(arr))
