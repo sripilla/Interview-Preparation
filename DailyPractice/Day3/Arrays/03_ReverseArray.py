ddaa762eba0e896c4bf871a7e637ac983887735a
sripilla <120908523+sripilla@users.noreply.github.com>
2026-08-31 20:25:58 +0530

    Update 03_ReverseArray.py

--- DailyPractice/Day3/Arrays/03_ReverseArray.py
+++ DailyPractice/Day3/Arrays/03_ReverseArray.py
@@ -15,10 +15,20 @@ Output:
 
 [5, 4, 3, 2, 1]
 """
-class Solution:
-    def reverse(self, nums: list[int]) -> None:
-        # Use two pointers
-        
-        # Swap elements from both ends
-        
-        pass
+def reverse(arr):
+    start = 0
+    end = len(arr) - 1
+
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+    return arr
+
+arr = []
+n = int(input("Enter the size of the array: "))
+print(f"Enter {n} elements:")
+for i in range(n):
+    arr.append(int(input()))
+
+print("Reversed array:", reverse(arr))
