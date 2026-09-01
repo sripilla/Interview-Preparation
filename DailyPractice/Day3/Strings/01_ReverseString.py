15aafa0637b4393f83fa17f319e5a342c13c0f4b
sripilla <120908523+sripilla@users.noreply.github.com>
2026-09-01 10:34:00 +0530

    Update 01_ReverseString.py

--- DailyPractice/Day3/Strings/01_ReverseString.py
+++ DailyPractice/Day3/Strings/01_ReverseString.py
@@ -16,10 +16,18 @@ Output:
 "olleh"
 """
 
-class Solution:
-    def reverseString(self, s: str) -> str:
-        # Initialize variables if needed
-        
-        # Reverse the string
-        
-        pass
+def rev(s):
+    start=0;
+    end=len(s)-1
+
+    while start<end:
+        s[start], s[end]= s[end], s[start]
+        start+=1
+        end-=1
+    return s
+s=[]
+n= int(input("Enter the size of the string:"))
+print(f"Enter the string elements:")
+for i in range(n):
+    s.append(input())
+print("Reverse of the string:", rev(s))
