85b1b654941e57a6ff920559e516ed1334df0f47
sripilla <120908523+sripilla@users.noreply.github.com>
2026-09-01 11:07:01 +0530

    Update 03_AnagramStrings.py

--- DailyPractice/Day3/Strings/03_AnagramStrings.py
+++ DailyPractice/Day3/Strings/03_AnagramStrings.py
@@ -17,14 +17,32 @@ Output:
 True
 """
 
-from collections import Counter
+def anagram(s1, s2):
+    if len(s1)!=len(s2):
+        return 0
+    else:
+        for i in range(len(s1)):
+            for j in range(len(s2)):
+                if s1[i]== s2[j]:
+                    s2.pop(j);
+                    break;
+    return 1
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        # If lengths are different
-        
-        # Count characters in both strings
-        
-        # Compare frequencies
-        
-        pass
+
+s1=[]
+s2=[]
+m= int(input("Enter the size of the string1:"))
+n= int(input("Enter the size of the string2:"))
+
+print(f"Enter {m} string1 elements:")
+for i in range(m):
+    s1.append(input())
+
+print(f"Enter {n} string2 elements:")
+for i in range(n):
+    s2.append(input())
+
+if anagram(s1, s2)== 1:
+    print("True")
+else:
+    print("False")
