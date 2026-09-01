ba34dd885e602b129205de1095d0843a31866b85
sripilla <120908523+sripilla@users.noreply.github.com>
2026-09-01 11:24:06 +0530

    Update 05_RemoveDuplicateCharacters.py

--- DailyPractice/Day3/Strings/05_RemoveDuplicateCharacters.py
+++ DailyPractice/Day3/Strings/05_RemoveDuplicateCharacters.py
@@ -16,16 +16,18 @@ Output:
 "progamin"
 """
 
-class Solution:
-    def removeDuplicates(self, s: str) -> str:
-        seen = set()
-        result = []
-        
-        # Traverse each character
-        
-        for ch in s:
-            # If character is not already seen
-            
-            pass
-        
-        return "".join(result)
+def removedup(s):
+    for i in range(len(s)-1):
+        for j in range(i+1, len(s)):
+            if s[i]==s[j]:
+                s.pop(j)
+                break;
+    return s
+
+s=[]
+n= int(input("Enter the size of the string:"))
+print(f"Enter {n} string elements:")
+for i in range(n):
+    s.append(input())
+
+print("string after removing duplicates:", removedup(s))
